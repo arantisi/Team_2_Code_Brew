c2828bb369bca5f652abd43bf558c505ca2eaaf2
Tyler Kukkola <tyler.kukkola@gmail.com>
2022-08-17 08:47:52 -0700

    Fixed typo

--- project_2/demo/src/main/java/com/example/demo/repositories/UserRepository.java
+++ project_2/demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -4,10 +4,8 @@ import com.example.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
    User findByUsername(String username);
-   User findById(int Id);\
+   User findById(int Id);
 }
